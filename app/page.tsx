1a434a642fc13edba80347d92847e05a87332dc3
Deven Shah <devenshah2018@gmail.com>
2025-07-07 15:53:07 -0700

    Build fixes.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { HeroSection } from "@/components/hero-section"
 import { AboutSection } from "@/components/about-section"
 import { ExperienceSection } from "@/components/experience-section"
 import { ProjectsSection } from "@/components/projects-section"
-import { PublicationsSection } from "@/components/publications-section"
 import { EducationSection } from "@/components/education-section"
 import { ContactSection } from "@/components/contact-section"
 
